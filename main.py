c98e17b2f9c779e0d13175939cd4d73565f91926
dqmdz-um <daniel.quinteros@um.edu.ar>
2022-09-13 21:17:37 -0300

    Add second function

--- main.py
+++ main.py
@@ -2,7 +2,5 @@ def suma(a: int, b: int):
     return a+b
 
 
-if __name__ == '__main__':
-    a = 1
-    b = 2
-    print(f'Suma {a} + {b} = {suma(a,b)}')
+def resta(a: int, b: int):
+    return a-b
--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 import unittest
 
-from main import suma
+from main import suma, resta
 
 
 class TestSuma(unittest.TestCase):
 
     def test_suma(self):
-        self.assertEqual(4, suma(3, 2))
+        self.assertEqual(4, suma(3, 1))
+
+    def test_resta(self):
+        self.assertEqual(1, resta(3, 2))
 
 
 if __name__ == '__main__':
